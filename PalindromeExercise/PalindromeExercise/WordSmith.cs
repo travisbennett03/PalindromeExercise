d92d79b5ca98b2e55e6b1acdf792e3ddc653af42
travisbennett03 <tbennett1996@yahoo.com>
2023-01-09 12:23:58 -0600

    initial commit

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word)
+        {
+            var reversed = "";
+
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                reversed += word[i];
+            }
+
+            if (reversed == word)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,10 +6,17 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("hello", false)]
+        public void Test1(string word, bool expected)
         {
+            var tester = new WordSmith();
 
+
+            var actual = tester.IsAPalindrome(word);
+
+            Assert.Equal(expected, actual);
         }
     }
 }
